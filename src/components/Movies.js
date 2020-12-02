a90e0f74f09ccfa6973080392ccfa9a0cb808888
NerlyTon <nerlyton@gmail.com>
2020-12-02 11:57:30 -0600

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        { movies.map((movie, i) => (
+          <div key={i}>
+            <h4>{movie.title}</h4>
+            <p>Time: {movie.time}</p>
+            <p>Genres:</p>
+            <ul>
+              {movie.genres.map((genre, i) => (
+                <li key={i}>
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            </div>
+        ))}
     </div>
   );
 };
