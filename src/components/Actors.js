c948f24357d3ef8b31dee186ecf854cf4cf1891a
Samantha Aleman <samantha.aleman25@gmail.com>
2020-12-03 19:31:15 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { actors } from '../data';
 
+
 const Actors = () => {
+
+  const renderActor = (actor) => {
+    return (
+      <div>
+        <h3>{actor.name}</h3>
+        <ul>
+          {actor.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  }
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => renderActor(actor))}
     </div>
   );
 };
-
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  const renderDirector = (director) => {
+    return (
+      <div>
+        <h2>{director.name}</h2>
+        <ul>
+          {director.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  }
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => renderDirector(director))}
     </div>
   );
 }
